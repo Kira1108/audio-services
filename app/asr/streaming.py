6b14ace55f253b6a120aa345d612d915e4fb59df
王欢 <wanghuanr@yxqiche.com>
2025-05-14 16:13:30 +0800

    Initial commit

--- app/asr/streaming.py
+++ app/asr/streaming.py
@@ -5,16 +5,66 @@ import numpy as np
 # from app.audio_utils import resample_audio_librosa
 from app.log import timer
 from app.config import get_config
+from funasr_onnx.paraformer_online_bin import Paraformer
 
 
 config = get_config()
 
+# @lru_cache(maxsize=None)
+# def load_model():
+#     return AutoModel(
+#         model=config.online_asr_model_path, 
+#         device = config.online_model_device,
+#         disable_update = True)
+
+# @dataclass
+# class ParaformerStreaming:
+#     chunk_ms:int = 600 
+#     encoder_chunk_look_back:int = 4
+#     decoder_chunk_look_back:int = 1
+    
+#     def __post_init__(self):
+#         self.model = load_model()
+#         self.cache = {}
+#         self.chunk_size = [0, int(self.chunk_ms / 60), int(self.chunk_ms / 120)]
+        
+
+#     def new(self):
+#         return ParaformerStreaming(
+#             chunk_ms=self.chunk_ms,
+#             encoder_chunk_look_back=self.encoder_chunk_look_back, 
+#             decoder_chunk_look_back=self.decoder_chunk_look_back)
+    
+#     @timer("ParaformerStreaming")
+#     def run(self, speech_chunk, sampling_rate:int =16000, is_final = False):
+        
+#         # if not sampling_rate != 16000:
+#         #     speech_chunk = resample_audio_librosa(speech_chunk, sampling_rate, 16000)
+            
+#         speech_chunk = np.array(speech_chunk).astype("float32")
+        
+#         res = self.model.generate(
+#             input=speech_chunk, 
+#             cache=self.cache, is_final=is_final, 
+#             chunk_size=self.chunk_size, 
+#             encoder_chunk_look_back=self.encoder_chunk_look_back, 
+#             decoder_chunk_look_back=self.decoder_chunk_look_back
+#         )
+#         return res
+
+from dataclasses import dataclass
+from functools import lru_cache
+
+
 @lru_cache(maxsize=None)
 def load_model():
-    return AutoModel(
-        model=config.online_asr_model_path, 
+    return Paraformer(
+        model_dir=config.online_asr_model_path, 
         device = config.online_model_device,
+        quantize = True,
+        chunk_size = [0,10,5],
         disable_update = True)
+
 
 @dataclass
 class ParaformerStreaming:
@@ -33,23 +83,29 @@ class ParaformerStreaming:
             chunk_ms=self.chunk_ms,
             encoder_chunk_look_back=self.encoder_chunk_look_back, 
             decoder_chunk_look_back=self.decoder_chunk_look_back)
-    
-    @timer("ParaformerStreaming")
+
     def run(self, speech_chunk, sampling_rate:int =16000, is_final = False):
-        
-        # if not sampling_rate != 16000:
-        #     speech_chunk = resample_audio_librosa(speech_chunk, sampling_rate, 16000)
             
         speech_chunk = np.array(speech_chunk).astype("float32")
         
-        res = self.model.generate(
-            input=speech_chunk, 
-            cache=self.cache, is_final=is_final, 
-            chunk_size=self.chunk_size, 
-            encoder_chunk_look_back=self.encoder_chunk_look_back, 
-            decoder_chunk_look_back=self.decoder_chunk_look_back
-        )
-        return res
+        res = self.model(
+            audio_in=speech_chunk, 
+            param_dict = dict(
+                cache = self.cache,
+                is_final = is_final,
+                encoder_chunk_look_back = self.encoder_chunk_look_back,
+                decoder_chunk_look_back = self.decoder_chunk_look_back
+            ))
+        if len(res) == 0:
+            text = " "
+        elif "preds" not in res[0]:
+            text = " "
+        else:
+            try:
+                text = res[0]['preds'][0]
+            except:
+                text = " "
+        return [{"text":text}]
     
 if __name__ == "__main__":
     import soundfile
